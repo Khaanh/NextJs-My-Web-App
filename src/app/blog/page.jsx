57c76afc4866c4730a0c38f069253dca2b0f0372
Khanh Nguyen <Khanh13Nguyen13@gmail.com>
2024-01-27 15:33:35 +0100

    Did: portfolio & blog categories

--- src/app/blog/page.jsx
+++ src/app/blog/page.jsx
@@ -2,12 +2,44 @@ import Link from "next/link";
 import styles from "./page.module.css";
 import Image from "next/image";
 
-const Blog = () => {
+async function getData() {
+	const res = await fetch("https://jsonplaceholder.typicode.com/posts", {
+		cache: "no-store",
+	});
+
+	if (!res.ok) {
+		throw new Error("Failed to fetch data");
+	}
+
+	return res.json();
+}
+
+const Blog = async () => {
+	const data = await getData();
+
 	return (
 		<div className={styles.mainContainer}>
-			<Link href="blog/test" className={styles.container} key="1">
-				<div>{/* <Image /> */}</div>
-			</Link>
+			{data.map((item) => (
+				<Link
+					href={`blog/${item.id}`}
+					className={styles.container}
+					key={item.id}
+				>
+					<div className={styles.mainContainer}>
+						<Image
+							src="https://img.freepik.com/free-vector/houses-concept-illustration_114360-807.jpg?w=740&t=st=1706362582~exp=1706363182~hmac=e157f0fcf2ee97674cc0e9882ef33b7e68145b1a941a4af600223c1f303e06dc"
+							alt=""
+							width={400}
+							height={250}
+							className={styles.image}
+						/>
+					</div>
+					<div className={styles.content}>
+						<h1 className={styles.title}>{item.title}</h1>
+						<p className={styles.desc}>{item.body}</p>
+					</div>
+				</Link>
+			))}
 		</div>
 	);
 };
--- src/app/dashboard/page.jsx
+++ src/app/dashboard/page.jsx
@@ -1,7 +1,41 @@
+"use client";
+import { useEffect, useState } from "react";
 import styles from "./page.module.css";
+import useSWR from "swr";
 
 const Dashboard = () => {
-	return <div>Dashboard</div>;
+	// const [data, setData] = useState([]);
+	// const [error, setError] = useState(false);
+	// const [isLoading, setIsLoading] = useState(true);
+
+	// useEffect(() => {
+	// 	const getData = async () => {
+	// 		const res = await fetch(
+	// 			`https://jsonplaceholder.typicode.com/posts/${id}`,
+	// 			{
+	// 				cache: "no-store",
+	// 			}
+	// 		);
+
+	// 		if (!res.ok) {
+	// 			setError(true);
+	// 		}
+	// 		const data = await res.json();
+
+	// 		setData(data);
+	// 		setIsLoading(false);
+	// 	};
+
+	// 	getData();
+	// }, []);
+
+	const fetcher = (...args) => fetch(...args).then((res) => res.json());
+	const { data, error, isLoading } = useSWR(
+		"https://jsonplaceholder.typicode.com/posts",
+		fetcher
+	);
+
+	return <div className={styles.container}>Dashboard</div>;
 };
 
 export default Dashboard;
